433267c1725abf94d21875db001850d3eea4fff5
AppInternet025 <appinternet025@gmail.com>
2026-06-18 12:32:54 -0400

    update_db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,6 @@
 // lib/db.js
 import { Pool } from 'pg'
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not defined in .env')
-}
-
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production'
